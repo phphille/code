da765db6361c18f0c812ddc0729cc772c1e1add3
phphille <fille_fillf@hotmail.com>
2020-02-27 17:23:18 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/NoTouchy/App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <img className="brand-logo" src="/brand-logo.png" alt="brand-logo" />
+      </header>
+      <div className="work-area"></div>
+    </div>
+  );
+}
+
+export default App;
